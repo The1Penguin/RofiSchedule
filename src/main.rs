e32daafca19213e88432defb3e7ef132ae919d3a
The1Penguin <jesper@acorneroftheweb.com>
2021-02-06 19:40:09 +0100

    Removed output and changed some formating

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ use chrono;
 fn main() {
     let uri = "https://cloud.timeedit.net/chalmers/web/public/ri.ics?sid=3&objects=196836.194&ox=0&p=0.m%2C20210321.x&e=210202&enol=t&ku=34920&k=E0128097C79129968D30F049CAEBC7B9";
     let icals = Calendar::new(uri);
-    
     let mut dir_entries: Vec<std::string::String> = Vec::new();
     let day = chrono::offset::Local::now();
+
     for ical in &icals.events{
         if day == ical.dtsart{
             let temp = &ical.summary;
@@ -24,11 +24,10 @@ fn main() {
     
     dir_entries.sort();
     
-    
     match rofi::Rofi::new(&dir_entries).run() {
-        Ok(choice) => println!("Choice: {}", choice),
-        Err(rofi::Error::Interrupted) => println!("Interrupted"),
-        Err(e) => println!("Error: {}", e)
+        Ok(_choice) => (),
+        Err(rofi::Error::Interrupted) => (),
+        Err(e) => println!("Error, {}", e)
     }
 }
 
