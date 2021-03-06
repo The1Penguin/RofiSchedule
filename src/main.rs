dce6b2f02f654044bc97d5b4a557628ada55b1d9
The1Penguin <jesper@acorneroftheweb.com>
2021-03-06 21:51:32 +0100

    Modified to make it more modular

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,22 @@ use chrono;
 fn main() {
     let uri = "https://cloud.timeedit.net/chalmers/web/public/ri.ics?sid=3&objects=196836.194&ox=0&p=0.m%2C20210321.x&e=210202&enol=t&ku=34920&k=E0128097C79129968D30F049CAEBC7B9";
     let icals = Calendar::new(uri);
-    let mut dir_entries: Vec<std::string::String> = Vec::new();
     let day = chrono::offset::Local::now();
+    let mut dir_entries: Vec<std::string::String> = generate_day(icals, day);
+    dir_entries.sort();
+    display(dir_entries);
+    }
+
+fn display(dir_entries:std::vec::Vec<std::string::String>){
+    match rofi::Rofi::new(&dir_entries).prompt("Schedule").run() {
+        Ok(_choice) => (),
+        Err(rofi::Error::Interrupted) => (),
+        Err(e) => println!("Error, {}", e)
+    }
+}
+
+fn generate_day(icals: Calendar, day:chrono::DateTime<chrono::Local>) -> std::vec::Vec<std::string::String> {
+    let mut dir_entries: Vec<std::string::String> = Vec::new();
 
     for ical in &icals.events{
         if day.format("%F").to_string() == ical.dtsart.format("%F").to_string(){
@@ -23,14 +37,5 @@ fn main() {
             }
         }
     }
-    
-    dir_entries.sort();
-    
-    match rofi::Rofi::new(&dir_entries).prompt("Schedule").run() {
-        Ok(_choice) => (),
-        Err(rofi::Error::Interrupted) => (),
-        Err(e) => println!("Error, {}", e)
-    }
+    return dir_entries
 }
-
-
